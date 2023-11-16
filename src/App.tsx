9ec8f90e59f32c8ae1bce8d20a5ee8ae8a0ed81f
Valentin Khokhlov <mananathen@gmail.com>
2023-11-16 10:19:11 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <img src={'https://cdn.docdoc.ru/doctor/1089768_20230613093706_small.jpg'} />
+      <img src={'https://cdn.docdoc.ru/doctor/912656_20211015145553_small.jpg'} />
+      <img src={'https://cdn.docdoc.ru/doctor/600802_20220601150219_small.jpg'} />
+    </>
+  )
+}
+
+export default App
